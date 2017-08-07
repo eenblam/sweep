ca37292a0523e615357f4472cfd8a9ec8efd7d9c
eenblam <ben.a.elam@gmail.com>
2017-08-06 21:56:47 -0700

    Mock game FSM

--- README.md
+++ README.md
@@ -7,3 +7,17 @@ Objects:
 - Game
 - Board
 - Cell
+
+## FSM
+Game object should be an FSM.
+
+- Start
+- Select
+    - Select (implement iteratively instead of recursively)
+    - Mine
+        - Start
+        - Exit
+    - Clear
+        - Start
+        - Exit
+
--- ms/game.py
+++ ms/game.py
@@ -2,6 +2,29 @@ class Game(object):
     def __init__(self):
         pass
 
+    def start(self):
+        """Start a new game."""
+        pass
+
+    def select(self):
+        """Select a coordinate pair to test."""
+        pass
+
+    def mine(self):
+        """End the game with a loss."""
+        pass
+
+    def clear(self):
+        """End the game with a win."""
+        pass
+
+    def exit(self):
+        """Clean up any resources and exit."""
+        pass
+
     def restart(self):
-        """Restart the current game."""
+        """Restart the current game.
+        
+        I.e. reset visibility but not mine distribution.
+        """
         pass
