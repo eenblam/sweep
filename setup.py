16d9e0936ebd21eb7c176a426b1b4c817bc510d0
eenblam <ben.a.elam@gmail.com>
2017-08-07 14:25:12 -0700

    Refactor for setuptools

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+setup(
+        name='sweep',
+        py_modules='sweep.py',
+        packages=find_packages(),
+        entry_points={
+            'console_scripts': [
+                'sweep = sweep:run'
+            ],
+        },
+)
--- sweep.py
+++ sweep.py
@@ -1,42 +1,4 @@
-#!/usr/bin/env python
-from sweep import game
-
-def usage(err_msg):
-    print(err_msg)
-    print("Usage:")
-    print("sweep.py <rows> <columns> [<probability>]")
-    sys.exit(1)
-
-def match_dimensions(args):
-    pass
-
-def match_dimensions_and_probability(s):
-    pass
+from sweep import run
 
 if __name__ == '__main__':
-    import sys
-    if len(sys.argv) not in (3, 4):
-        usage("Wrong number of arguments")
-
-    try:
-        rows = int(sys.argv[1])
-    except ValueError:
-        usage("Could not parse number of rows")
-    try:
-        cols = int(sys.argv[2])
-    except ValueError:
-        usage("Could not parse number of columns")
-
-    if len(sys.argv) == 4:
-        try:
-            probability = float(sys.argv[3])
-        except ValueError:
-            usage("Could not parse probability as float")
-
-        if probability > 1 or probability <= 0:
-            usage("Probability must be between 0 and 1, exclusively")
-    else:
-        probability = 0.2
-
-    game = game.Game(rows, cols, probability)
-    game.start()
+    run()
