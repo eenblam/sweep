b4f5c1671bf88b8aabdaae13865e752b3eec74ca
benelam <benelam@eff.org>
2022-10-27 09:45:36 -0700

    Improve usage and docs

--- sweep/__init__.py
+++ sweep/__init__.py
@@ -5,8 +5,23 @@ from .game import Game
 
 def usage(err_msg):
     print(err_msg)
+    print()
     print("Usage:")
     print("sweep.py <rows> <columns> [<probability>]")
+    print()
+    print("Play:")
+    print("Check a cell to see how many mines are adjacent to it.")
+    print("...but try not to check cells containing mines!")
+    print("Flag any cells you're sure contain mines.")
+    print("Flag all the mines (with no mistakes) without triggering one to win.")
+    print()
+    print("Note: (0,0) is the top-left cell.")
+    print()
+    print("Symbols:")
+    print("0-8 : the number of adjacent mines")
+    print("F : a cell you've flagged. Flag again to remove.")
+    print("@ : a mine")
+    print("% : a cell you selected that contained a mine")
     sys.exit(1)
 
 def run():
